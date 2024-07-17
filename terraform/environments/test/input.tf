bd4f84e43e3487c02a6ee34d51ebbdc5baca4c6b
Thien Nguyen <nguyennhutamthienbk2011@gmail.com>
2024-07-17 21:31:49 +0700

    update pipelines

--- terraform/environments/test/input.tf
+++ terraform/environments/test/input.tf
@@ -1,9 +1,9 @@
 # Azure GUIDS
-variable "subscription_id" {}
-variable "client_id" {}
-variable "client_secret" {}
-variable "tenant_id" {}
-variable "access_key" {}
+# variable "subscription_id" {}
+# variable "client_id" {}
+# variable "client_secret" {}
+# variable "tenant_id" {}
+# variable "access_key" {}
 
 # Resource Group/Location
 variable "location" {}
